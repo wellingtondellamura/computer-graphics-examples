a8f39d0c76d82d27c9f7a6feca5357b629d8e0b6
wellingtondellamura <della.mura@gmail.com>
2017-06-13 22:40:48 -0300

    updates

--- html5/canvas/intro-canvas.js
+++ html5/canvas/intro-canvas.js
@@ -55,3 +55,118 @@ ctx.lineWidth=10; //https://www.w3schools.com/tags/playcanvas.asp?filename=playc
 
 //Desenhar linhas de um retângulo
 ctx.strokeRect(20,20,150,100);
+
+//Desenhar formas geométricas
+   //RETANGULO
+   ctx.rect(20,20,150,100); //retangulo (x,y,w,h);
+   ctx.stroke();
+
+   fillRect()
+   strokeRect()
+   clearRect()
+
+   //CIRCULO
+   ctx.beginPath();
+   ctx.arc(100,75,50,0,2*Math.PI); //https://www.w3schools.com/tags/canvas_arc.asp
+   //center(100,75) startAngle(0) endAngle(2*pi)
+   ctx.stroke();
+
+   //POLIGONOS
+   //triangulo
+   var c = document.getElementById("myCanvas");
+   var ctx = c.getContext("2d");
+   ctx.beginPath();
+   ctx.moveTo(50,50);
+   ctx.lineTo(90,100);
+   ctx.lineTo(10,100);
+   ctx.lineTo(50,50);
+   ctx.stroke();
+   ctx.fill();
+
+
+ctx.lineWidth = "1"; //largura da linha
+ctx.strokeStyle='rgb(255,0,0)'; //cor da linha
+ctx.stroke(); //linha
+
+ctx.fillStyle='rgb(255,0,0)'; //cor do preenchimento
+ctx.fill();   //preenchimento
+
+
+//Personalizar o preenchimento
+   //color
+   var c = document.getElementById('myCanvas');
+   var ctx = c.getContext('2d');
+
+   ctx.rect(10,10,400,400);
+   ctx.fillStyle='rgb(255,0,0)'; //'rgb(255,0,0)' | 'red' | '#FF0000'
+   ctx.fill();
+
+   //gradient
+      //linear
+      var c = document.getElementById('myCanvas');
+      var ctx = c.getContext('2d');
+
+      var gradient = ctx.createLinearGradient(0,0,170,0);
+      gradient.addColorStop("0","magenta");
+      gradient.addColorStop("0.5","blue");
+      gradient.addColorStop("1.0","red");
+
+      ctx.rect(10,10,400,400);
+      ctx.fillStyle=gradient;
+      ctx.fill();
+      //radial
+      //https://www.w3schools.com/tags/canvas_createradialgradient.asp
+      var c=document.getElementById("myCanvas");
+      var ctx=c.getContext("2d");
+
+      var grd=ctx.createRadialGradient(75,50,5,90,60,100);
+      // x0 	The x-coordinate of the starting circle of the gradient
+      // y0 	The y-coordinate of the starting circle of the gradient
+      // r0 	The radius of the starting circle
+      // x1 	The x-coordinate of the ending circle of the gradient
+      // y1 	The y-coordinate of the ending circle of the gradient
+      // r1 	The radius of the ending circle
+      grd.addColorStop(0,"red");
+      grd.addColorStop(1,"white");
+
+      // Fill with gradient
+      ctx.fillStyle=grd;
+      ctx.fillRect(10,10,150,100);
+
+   //pattern
+   var img = new Image();
+   img.src = 'http://bgpatterns.com/patterns/v/x/4/pattern.png';
+
+   var c=document.getElementById("myCanvas");
+   var ctx=c.getContext("2d");
+
+   var pat=ctx.createPattern(img,"repeat");
+   ctx.rect(10,10,400,400);
+   ctx.fillStyle=pat;
+   ctx.fill();
+
+//Sombra
+   var c = document.getElementById('myCanvas');
+   var ctx = c.getContext('2d');
+
+   ctx.shadowBlur=20;
+   ctx.shadowColor="black";
+   ctx.shadowOffsetX = 15;
+   ctx.shadowOffsetY = 15;
+
+   ctx.rect(10,10,400,400);
+   ctx.fillStyle='rgb(255,0,0)'; //'rgb(255,0,0)' | 'red' | '#FF0000'
+   ctx.fill();
+
+//Desenhar imagem
+
+   var c = document.getElementById('myCanvas');
+   var ctx = c.getContext('2d');
+
+
+   var img = new Image();
+   img.src = 'https://image.freepik.com/fotos-gratis/sorriso-de-lhama_2270977.jpg';
+
+   ctx.drawImage(img,10,10, 200, 200);
+
+  
--- html5/canvas/main.js
+++ html5/canvas/main.js
@@ -1,16 +1,31 @@
-function draw(x,y){
+function drawTriangle(x,y){
   var cv = document.getElementById("cv");
   var ctx = cv.getContext("2d");
   ctx.beginPath();
   ctx.moveTo(x,y);
-  ctx.lineTo(x-50,y+100);
-  ctx.lineTo(x+50,y+100);
+  ctx.lineTo(x-150,y+200);
+  ctx.lineTo(x+150,y+200);
   ctx.lineTo(x,y);
+
+
+  ctx.shadowBlur=50;
+  ctx.shadowColor="black";
+  ctx.shadowOffsetX = 50;
+  ctx.shadowOffsetY = 50;
+
   ctx.strokeStyle='rgb(255,0,0)';
-  //ctx.lineCap = 'round';
-  ctx.lineWidth = 5;
-  ctx.lineJoin = 'miter';
+  ctx.lineCap = 'round';
+  ctx.lineWidth = 6;
+  ctx.lineJoin = 'round';
   ctx.miterLimit = 5;
   ctx.stroke();
-  //ctx.fill();
+  ctx.fillStyle='rgb(0,255,0)';
+  ctx.fill();
+}
+function drawRect(x,y){
+  var c = document.getElementById('cv');
+  var ctx = c.getContext('2d');
+  var img = new Image();
+  img.src = 'https://image.freepik.com/fotos-gratis/sorriso-de-lhama_2270977.jpg';
+  ctx.drawImage(img,x,y,img.width, img.height);
 }
